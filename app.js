dc3b5366cca110c02da345005608b40127bf6c59
Loiane Groner <loianeg@gmail.com>
2011-08-07 18:40:13 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+Ext.Loader.setConfig({enabled: true});
+
+Ext.require([
+    'Ext.grid.*',
+    'Ext.data.*',
+    'ux.grid.HeaderToolTip'
+]);
+
+Ext.onReady(function() {
+    
+    var myData = [
+        ['3m Co'],
+        ['Alcoa Inc'],
+        ['Altria Group Inc'],
+        ['American Express Company'],
+        ['American International Group, Inc.'],
+        ['AT&T Inc.'],
+        ['Boeing Co.'],
+        ['Caterpillar Inc.'],
+        ['Citigroup, Inc.'],
+        ['E.I. du Pont de Nemours and Company'],
+        ['Exxon Mobil Corp'],
+        ['General Electric Company']
+    ];
+
+    var store = Ext.create('Ext.data.ArrayStore', {
+        fields: [
+           {name: 'company'}
+        ],
+        data: myData
+    });
+    
+    var grid = Ext.create('Ext.grid.Panel', {
+        store: store,
+        plugins: ['headertooltip'],
+        columns: [
+            {
+                text     : 'Company',
+                flex     : 1,
+                sortable : false,
+                dataIndex: 'company',
+                tooltip: 'Some tooltip'
+            }
+        ],
+        height: 200,
+        width: 200,
+        title: 'Grid with Header Tooltip',
+        renderTo: 'grid-example',
+        viewConfig: {
+            stripeRows: true
+        }
+    });
+});
+
---
+++ readme.md
@@ -0,0 +1,23 @@
+# Ext JS 4 Grid with Header Tooltip
+
+Demonstrates how to add a tooltip to a grid header in Ext JS 4.
+
+## Plugin - Sencha Forum
+
+O copied the plugin originally from this url
+
+* http://www.sencha.com/forum/showthread.php?132637-Ext.ux.grid.HeaderToolTip
+
+## Post - Blog - Step by Step
+
+* comming soon
+
+## How to execute
+
+* Simply open index.html
+
+## More Ext JS Examples and Tutorials
+
+* Loiane Groner
+* http://loianegroner.com (English)
+* http://loiane.com (Portuguese)
