c917431f45428973aa3ebcd5483834159e1fa9cc
Loiane Groner <loianeg@gmail.com>
2011-10-04 07:39:26 -0300

    fixed namespace

--- ux/grid/HeaderToolTip.js
+++ ux/grid/HeaderToolTip.js
@@ -1,13 +1,13 @@
 /**
- * @class ux.grid.HeaderToolTip
- * @namespace ux.grid
+ * @class Ext.ux.grid.HeaderToolTip
+ * @namespace Ext.ux.grid
  *
  *  Text tooltips should be stored in the grid column definition
  *  
  *  Sencha forum url: 
  *  http://www.sencha.com/forum/showthread.php?132637-Ext.ux.grid.HeaderToolTip
  */
-Ext.define('ux.grid.HeaderToolTip', {
+Ext.define('Ext.ux.grid.HeaderToolTip', {
     alias: 'plugin.headertooltip',
     init : function(grid) {
         var headerCt = grid.headerCt;
